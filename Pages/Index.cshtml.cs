e71322af0017b6d96db566085720e71a71c990e1
AAndrzejczuk <ag2rio@gmail.com>
2021-05-15 19:07:35 +0200

    Add sending Comment/Opinion message to Azure Service Bus

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,28 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using WebCommentsSection.Models;
 
 namespace WebCommentsSection.Pages {
     public class IndexModel : PageModel {
 
         private readonly WebCommentsSection.Data.WebCommentsSectionContext _context;
+        const string ServiceBusConnectionString = "Endpoint=sb://service-bus-aa-15052021.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=P9sILgyUFyGFiobVRE7EEKz0X68pWTM19q+M3LyV7Ko=";
+        private const string QueueName = "opinions";
+        private readonly IQueueClient _queueClient; 
 
         public IndexModel(WebCommentsSection.Data.WebCommentsSectionContext context) {
             _context = context;
+            _queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
         }
         public IList<Comment> Comments { get; set; }
         
@@ -29,7 +38,7 @@ namespace WebCommentsSection.Pages {
             Comments = comments.ToList();
         }
 
-        public IActionResult OnPost() { // moznaby tutaj dodac Comment jako parametr OnPost(Comment Comment)
+        public async Task<IActionResult> OnPost() { // moznaby tutaj dodac Comment jako parametr OnPost(Comment Comment)
             if (!ModelState.IsValid) {
                 // may save that in session variable but it uses server memory too much 
                 var comments = from c in _context.Comment
@@ -47,6 +56,21 @@ namespace WebCommentsSection.Pages {
             Comment.Timestamp = DateTime.Now;
             _context.Comment.Add(Comment);
             _context.SaveChanges();
+
+            // Azure Service Bus connection
+            try
+            {
+                string message = JsonConvert.SerializeObject(Comment);
+                var encodedMessage = new Message(Encoding.UTF8.GetBytes(message));
+                Console.WriteLine($"Sending message: {message}");
+                await _queueClient.SendAsync(encodedMessage);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+            await _queueClient.CloseAsync();
+            // Azure end
 
             Comment.Name = null;
             Comment.Message = null;
